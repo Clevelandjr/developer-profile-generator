01e365dd178209fea974e4379b0a38fabf17b18b
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MacBook-Pro.local>
2019-12-19 00:19:54 -0500

    added questions to indexjs

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var inquirer = require("inquirer");
+var fs =require('fs');
+var generateHTML = require("./generateHTML");
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is you github username?"
+    },
+    {
+    type: "list",
+    message: "What is your favorite color?",
+    name: "color",
+    choices: [
+        "white",
+        "blue",
+        "pink",
+        "orange",
+        "purple"
+        "red",
+        "brown"
+    ]
+    }
+])
+
 const questions = [
   
 ];
